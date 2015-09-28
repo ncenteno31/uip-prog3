7c8f0292a578298dfd063fe73165b370039e15c4
ncenteno31 <ncenteno31@outlook.com>
2015-09-27 21:44:21 -0500

    añadiendo tarea 2

---
+++ tareas/tarea2.py
@@ -0,0 +1,8 @@
+distancia = 120
+tiempo = 1*60
+velocidad = distancia/tiempo
+distancia2 = 60
+tiempor = distancia2/velocidad
+tiempor_segun = tiempor*60
+
+print('El tiempo que tarda una Ambulancia en recorrer 60Km a una velocidad de 120Km/h es de '+str(tiempor_segun)+' segundo')
